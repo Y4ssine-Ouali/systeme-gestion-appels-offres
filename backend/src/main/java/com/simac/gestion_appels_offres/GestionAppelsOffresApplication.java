3dc5ec2f10ae28cf7934f57ccff7e277ee04bb75
Yassine Ouali <yassine.ouali@etudiant-fst.utm.tn>
2026-02-19 13:17:22 +0100

    Initial commit

---
+++ backend/src/main/java/com/simac/gestion_appels_offres/GestionAppelsOffresApplication.java
@@ -0,0 +1,13 @@
+package com.simac.gestion_appels_offres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAppelsOffresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAppelsOffresApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/simac/gestion_appels_offres/GestionAppelsOffresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simac.gestion_appels_offres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAppelsOffresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
